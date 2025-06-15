22053984896bab8fcd868fcfba1fb41ad857d365
ketki kumeriya <111123802+ketkikumeriya@users.noreply.github.com>
2025-06-15 20:03:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+async function fetchAndLog() {
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+fetchAndLog();
